198647146194e98ecc6ca8119b268b8ec029c6a1
David Avendaño <d.avendano.c@hotmail.com>
2021-03-20 19:26:17 -0600

    First commit of this repo

---
+++ lib/description_place.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class DescriptionPlace extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+    final text = Container(
+      margin: EdgeInsets.only(top: 10.0, left: 20.0, right: 20.0),
+      child: Text(
+        " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        style: TextStyle(fontSize: 16.0),
+        textAlign: TextAlign.justify,
+      ),
+    );
+
+    final star = Container(
+      margin: EdgeInsets.only(top: 323.0, right: 3.0),
+      child: Icon(Icons.star, color: Color(0xFFf2C611)),
+    );
+
+    final title_stars = Row(
+      children: [
+        Container(
+          margin: EdgeInsets.only(
+            top: 320.0,
+            left: 20.0,
+            right: 20.0,
+          ),
+          child: Text(
+            "Duwili Ella",
+            style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w900),
+            textAlign: TextAlign.left,
+          ),
+        ),
+        Row(
+          children: [
+            star,
+            star,
+            star,
+            star,
+            star,
+          ],
+        )
+      ],
+    );
+    return Column(
+      children: [title_stars, text],
+    );
+  }
+}
