b7ea9ba66711f44145482cbe6d92ab58f5fd3afe
David Avendaño <d.avendano.c@hotmail.com>
2021-05-16 12:17:58 -0500

    Solved iOS build issues

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/davidavendanojr/Utils/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/davidavendanojr/Documents/Platzi/Curso Flutter/trips_app"
+export "FLUTTER_APPLICATION_PATH=/Users/davidavendanojr/Documents/Platzi/CursoFlutter/trips_app"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
